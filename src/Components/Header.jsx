1775eadd4cf6876b7e2133a416e015cdf9047776
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-03-23 05:10:53 -0600

    Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,34 @@
+import '../assets/styles/header.css';
+
+import { Outlet, Link } from "react-router-dom"
+
+function Header() {
+
+    return(
+
+        <div>
+
+            <nav className="navbar">
+                <div className="inner-width">
+                    <a href="/" className="logo"></a>
+                    <h1>ESNTLS</h1>
+                    <br></br>
+                    <button className="menu-toggler"></button>
+
+                    <div className="navbar-menu">
+                        <Link to="/">Home</Link>
+                        <Link to="#">Login</Link>
+
+                    </div>
+
+                </div>
+
+            </nav> <Outlet/>
+
+        </div>
+
+    )
+
+}
+
+export default Header;
