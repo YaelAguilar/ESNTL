88d1569c0d25cee1e980e0cbb718a6acc46d1b92
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-03-23 05:10:43 -0600

    Home

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import Header from "../Components/Header";
+
+function Home(){
+
+    return(
+
+     <>
+        <Header></Header>
+        <h1>Home</h1>
+     </>
+
+    )
+
+}
+
+export default Home;
